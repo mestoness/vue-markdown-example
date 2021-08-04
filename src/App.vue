69a7fec023202b84c3a74dbbfdaf940f65970741
Ahmet Baki Memiş <69093842+mestoness@users.noreply.github.com>
2021-08-04 17:43:44 +0300

    Added Grid System

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
-    <div style="text-align:center;margin:1rem 0">
+    <div style="text-align:center;margin:2rem 0">
       <b style="color:white;"
         ><a href="https://github.com/mestoness" style="color:white;"
           >MestOness</a
         ></b
       >
     </div>
-    <textarea v-model="source" rows="30"></textarea>
-    <vue-markdown
-      :source="source"
-      :show="show"
-      :html="html"
-      :breaks="breaks"
-      :linkify="linkify"
-      :emoji="emoji"
-      :typographer="typographer"
-      :toc="toc"
-      class="preview"
-    ></vue-markdown>
+    <div class="row">
+      <div class="col s6">
+        <textarea v-model="source"></textarea>
+      </div>
+
+      <div class="col s6">
+        <vue-markdown
+          :source="source"
+          :show="show"
+          :html="html"
+          :breaks="breaks"
+          :linkify="linkify"
+          :emoji="emoji"
+          :typographer="typographer"
+          :toc="toc"
+          class="preview"
+        ></vue-markdown>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -207,15 +214,104 @@ body {
   font-weight: 700;
 }
 textarea {
-  width: 90%;
   border: 1px solid grey;
   padding: 15px;
   background-color: rgb(42, 42, 42);
   color: white;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   margin-bottom: 1rem;
+  height: 40vh;
+  resize: inherit;
 }
 textarea:focus {
   outline: 0;
+}
+hr {
+  margin: 1rem 0;
+  color: inherit;
+  background-color: currentColor;
+  border: 0;
+  opacity: 0.25;
+}
+
+hr:not([size]) {
+  height: 1px;
+}
+
+h6,
+.h6,
+h5,
+.h5,
+h4,
+.h4,
+h3,
+.h3,
+h2,
+.h2,
+h1,
+.h1 {
+  margin-top: 10px;
+  margin-bottom: 0.5rem;
+  font-weight: 500;
+  line-height: 1.5;
+}
+
+h1,
+.h1 {
+  font-size: calc(1.375rem + 1.5vw);
+}
+@media (min-width: 1200px) {
+  h1,
+  .h1 {
+    font-size: 2.5rem;
+  }
+}
+
+h2,
+.h2 {
+  font-size: calc(1.325rem + 0.9vw);
+}
+@media (min-width: 1200px) {
+  h2,
+  .h2 {
+    font-size: 2rem;
+  }
+}
+
+h3,
+.h3 {
+  font-size: calc(1.3rem + 0.6vw);
+}
+@media (min-width: 1200px) {
+  h3,
+  .h3 {
+    font-size: 1.75rem;
+  }
+}
+
+h4,
+.h4 {
+  font-size: calc(1.275rem + 0.3vw);
+}
+@media (min-width: 1200px) {
+  h4,
+  .h4 {
+    font-size: 1.5rem;
+  }
+}
+
+h5,
+.h5 {
+  font-size: 1.25rem;
+}
+
+h6,
+.h6 {
+  font-size: 1rem;
+}
+
+p {
+  margin-top: 0;
+  margin-bottom: 1rem;
 }
 </style>
